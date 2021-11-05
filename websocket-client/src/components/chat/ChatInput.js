f7a5d2c4bfecf1114b418b02c6a5663d73245c5a
Hahn Jiho <ferrarijm@naver.com>
2021-11-05 14:28:35 +0900

    closes #8

--- websocket-client/src/components/chat/ChatInput.js
+++ websocket-client/src/components/chat/ChatInput.js
@@ -1,13 +1,21 @@
-import {useState} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import * as Type from './Message';
 
 const ChatInput = (props) => {
 
     const [input, setInput] = useState("")
+    const textInput = useRef(null)
+
+    useEffect(()=>{
+        textInput.current.focus()
+    }, [])
 
     const handleKeypress = e => {
         if(e.which === 13 && !e.shiftKey && input !== ""){
-            submitInput(e)
+            e.preventDefault()
+            submitInput()
+            e.target.value = ""
+            setInput("")
         }
     }
 
@@ -17,30 +25,30 @@ const ChatInput = (props) => {
         setInput(e.target.value)
     }
 
-    const handleSubmit = e => {
+    const handleClick = e => {
         e.preventDefault();
         if(input === "")
             return
-        submitInput(e)
+
+        submitInput()
+        textInput.current.value = ""
+        setInput("")
     }
 
-    const submitInput = e => {
+    const submitInput = () => {
         let jsonMessage = JSON.stringify({
             username: props.username,
             content: input,
             date: new Date(),
             type: Type.MESSAGE
         })
-        e.preventDefault()
-        e.target.value = ""
-        setInput("")
         props.socket.send(jsonMessage)
     }
 
     return (
         <div className="ChatInput">
-            <form onSubmit={handleSubmit}>
-                <textarea type="text" name="message" className="input" onChange={handleChange} onKeyPress={handleKeypress}/>
+            <form onSubmit={handleClick}>
+                <textarea ref={textInput} type="text" name="message" className="input" onChange={handleChange} onKeyPress={handleKeypress}/>
                 <button type="submit" className="button">Send</button>
             </form>
         </div>
