e06644d4f7f75452ee127a42ea5258319f54a742
Hahn Jiho <ferrarijm@naver.com>
2022-03-12 19:08:58 +0900

    Add gateway module

---
+++ chat-gw/src/main/java/com/jonathan/chat/gw/GWApplication.java
@@ -0,0 +1,11 @@
+package com.jonathan.chat.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GWApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GWApplication.class);
+    }
+}
